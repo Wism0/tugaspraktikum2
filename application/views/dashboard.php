d744b9c48af139b4fddb3d837012e4459aa706f5
Wism0 <anjar321998@gmail.com>
2019-10-22 14:02:18 +0700

    tugas2

---
+++ application/views/dashboard.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body align="center">
+    <h1>Rodok Telat</h1>    
+    <?php
+    $nama = "Wismo Ginanjar";
+    $title = "tugas praktikum2";
+    ?>
+
+    <p>nama : <?php echo $nama?></p>
+    <p>title : <?php echo $title?></p>
+
+</body>
+</html>
